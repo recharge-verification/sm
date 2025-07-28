922ed84b9117a674652949c4adf0537bb4fb88fe
recharge-verification <bryanherman261@gmail.com>
2025-07-28 03:39:36 +0100

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/users/Login";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/users/Login"
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Routes>
-        <Route path="*" element={<Login />} />
+        <Route path="/users/login" element={<Login />} />
+        {/* Ajoutez ceci pour gérer les 404 */}
+        <Route path="*" element={<Navigate to="/users/login" replace />} />
       </Routes>
     </Router>
-  );
+  )
 }
-
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+   base: '/', // Important pour les routes
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
